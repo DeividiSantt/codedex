da514a3f00cb5096b667d17a47161a678c8615ef
deividi47 <defaultmax993@gmail.com>
2025-07-24 13:26:05 -0300

    trifecta

---
+++ JavaScript/Trifecta-sites/ex002/script.js
@@ -0,0 +1,7 @@
+// Write code below 💖
+
+let drakePicTwo = getElementById("drakePicTwo");
+let heading = document.querySelector("h1")
+
+drakePicTwo.querySelector("img").src = "https://i.imgur.com/RGbh6zY.png";
+heading.getElementById("heading-2").innerText = "Learn By Doing";
