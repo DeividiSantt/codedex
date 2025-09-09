49ba315ea483ad46bcb321aec26c5e1f2005dafb
deividi47 <defaultmax993@gmail.com>
2025-09-08 21:52:42 -0300

    s

---
+++ JavaScript/RevisãoJS/revisaoex001.js
@@ -0,0 +1,6 @@
+let nome = 'Davi';
+let idade = 15;
+let cidade = 'Rio Branco'
+const pi = 3.14;
+const cores = ['roxo', 'marrom', 'vermelho'];
+const pessoa = [nome, idade, cidade];
---
+++ JavaScript/RevisãoJS/revisaoex002.js
@@ -0,0 +1,26 @@
+let numA = 5;
+let numB = 10;
+let soma = numA + numB;
+
+const verify = 10 > 5;
+
+function podeEntrar(idade, cidade, pais){
+
+let idade = 15;
+let cidade = "Rio Branco".toLowerCase();
+let pais = "Estados Unidos".toLowerCase();
+
+if(idade > 18 && cidade === "rio branco"){
+return true;
+    }
+
+if(idade > 18 || pais === "estados unidos"){
+return true;
+    }
+    return false;
+}
+
+let verifyValores = "5"
+let verifyValores2 = 5
+
+let comparação = verifyValores === verifyValores2
